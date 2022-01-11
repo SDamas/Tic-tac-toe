f154c6630012f837d25347b14acccf3aff44e743
Samad <samad.damas@hotmail.com>
2022-01-11 19:46:21 -0300

    Setup of the file to start the project

---
+++ tic-tac-toe.py
@@ -0,0 +1,7 @@
+#This is my tic-tac-toe game refresher
+#I should import grid or something similiar to work on my project
+#Check why I'm receiving the error message when I try to commit this file.
+
+player_1 = 'x'
+player_2 = 'y'
+
